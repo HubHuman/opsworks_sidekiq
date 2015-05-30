c100dcbaa8c926253e189f6421bdc639667db0cb
John C. Bland II <johncblandii@gmail.com>
2015-05-30 13:06:58 -0500

    General file clean-up

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,10 +1,9 @@
 # Adapted from unicorn::rails: https://github.com/aws/opsworks-cookbooks/blob/master/unicorn/recipes/rails.rb
 
-include_recipe "opsworks_sidekiq::service"
+include_recipe 'opsworks_sidekiq::service'
 
 # setup sidekiq service per app
 node[:deploy].each do |application, deploy|
-
   if deploy[:application_type] != 'rails'
     Chef::Log.debug("Skipping opsworks_sidekiq::setup application #{application} as it is not a Rails app")
     next
@@ -30,11 +29,10 @@ node[:deploy].each do |application, deploy|
 
   if node[:sidekiq][application]
 
-    workers = node[:sidekiq][application].to_hash.reject {|k,v| k.to_s =~ /restart_command|syslog/ }
+    workers = node[:sidekiq][application].to_hash.reject { |k, _v| k.to_s =~ /restart_command|syslog/ }
     config_directory = "#{deploy[:deploy_to]}/shared/config"
 
     workers.each do |worker, options|
-
       # Convert attribute classes to plain old ruby objects
       config = options[:config] ? options[:config].to_hash : {}
       config.each do |k, v|
@@ -47,14 +45,14 @@ node[:deploy].each do |application, deploy|
       end
 
       # Generate YAML string
-      yaml = YAML::dump(config)
+      yaml = YAML.dump(config)
 
       # Convert YAML string keys to symbol keys for sidekiq while preserving
       # indentation. (queues: to :queues:)
-      yaml = yaml.gsub(/^(\s*)([^:][^\s]*):/,'\1:\2:')
+      yaml = yaml.gsub(/^(\s*)([^:][^\s]*):/, '\1:\2:')
 
       (options[:process_count] || 1).times do |n|
-        file "#{config_directory}/sidekiq_#{worker}#{n+1}.yml" do
+        file "#{config_directory}/sidekiq_#{worker}#{n + 1}.yml" do
           mode 0644
           action :create
           content yaml
@@ -64,14 +62,12 @@ node[:deploy].each do |application, deploy|
 
     template "#{node[:monit][:conf_dir]}/sidekiq_#{application}.monitrc" do
       mode 0644
-      source "sidekiq_monitrc.erb"
-      variables({
-        :deploy => deploy,
-        :application => application,
-        :workers => workers,
-        :syslog => node[:sidekiq][application][:syslog]
-      })
-      notifies :reload, resources(:service => "monit"), :immediately
+      source 'sidekiq_monitrc.erb'
+      variables(deploy: deploy,
+                application: application,
+                workers: workers,
+                syslog: node[:sidekiq][application][:syslog])
+      notifies :reload, resources(service: 'monit'), :immediately
     end
 
   end
